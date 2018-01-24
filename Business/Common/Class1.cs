a1d3768871426bc1637b7e9a3851687ef6ed520f
Pedro Ramos <pmmramos@outlook.com>
2018-01-24 13:40:07 +0100

    InitialLoad

---
+++ Business/Common/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    public class Class1
+    {
+
+        static void Main(string[] args)
+        {
+            using (var db = new Common.ExpensesV2Entities())
+            {
+                var query = from x in db.Accounts
+                            orderby x.id
+                            select x;
+
+                foreach (var item in query)
+                {
+                    Console.WriteLine(item.id + "-" + item.CodCurrency + "-" + item.CodAccountType);
+                }
+
+
+            }
+        }
+    }
+}
---
+++ Tester/ConsoleTester/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleTester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Common com = new Common();
+            using (var db = new Common.ExpensesV2Entities())
+            {
+                /*var query = from Common.Accounts in db.Accounts
+                            select Common.Accounts.Banks;*/
+                var query = from Accounts in db.Accounts
+                            select Accounts.Banks.Description;
+
+                foreach (var item in query)
+                {
+                    Console.WriteLine("->" + item);
+                }
+                Console.ReadLine();
+            }
+        }
+    }
+}
